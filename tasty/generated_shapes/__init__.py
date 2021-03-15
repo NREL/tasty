3363cb052602ca8494fb7eb0cab75ea4e18e2ad0
corymosiman12 <cory.mosiman12@gmail.com>
2021-03-15 07:15:44 -0400

    remove old functions

--- tasty/generated_shapes/__init__.py
+++ tasty/generated_shapes/__init__.py
@@ -4,13 +4,6 @@ from rdflib import Graph
 from rdflib.util import guess_format
 
 generated_dir = os.path.dirname(__file__)
-core_file = os.path.join(generated_dir, 'haystack_core_v1.ttl')
-core_shapes = Graph()
-core_shapes.parse(core_file, format=guess_format(core_file))
-
-core_file2 = os.path.join(generated_dir, 'haystack_core_v2.ttl')
-core_shapes2 = Graph()
-core_shapes2.parse(core_file2, format=guess_format(core_file2))
 
 
 def load_all_shapes_and_merge():
